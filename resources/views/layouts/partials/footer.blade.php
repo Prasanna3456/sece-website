2afad1d9472f72595952aef3e8d2d52d161371ee
Thirumalai15 <m.thirurahul@gmail.com>
2022-10-29 23:01:12 +0530

    footer changes

--- resources/views/layouts/partials/footer.blade.php
+++ resources/views/layouts/partials/footer.blade.php
@@ -1,16 +1,20 @@
 <footer class="bg-[#121211] body-font bottom-0 relative">
     <div class="bg-black bg-opacity-30 ">
-        <div class="container mx-auto py-4 px-5 flex flex-wrap flex-col sm:flex-row">
-            <p class="text-gray-400 text-sm text-center sm:text-left">© {{ date('Y') }} Quintessence
+        <div class="container mx-auto py-4 px-5 flex  w-full justify-evenly flex-wrap flex-col sm:flex-row">
+            <div class="text-gray-400 text-sm text-center sm:text-left">© {{ date('Y') }} Quintessence
                 {{-- <a href="https://twitter.com/knyttneve" rel="noopener noreferrer" class="text-gray-500 ml-1"
                     target="_blank">@knyttneve</a> --}}
-            </p>
-            <div class="sm:text-center md:text-center">
-            <p class="text-gray-400 text-sm sm:justify-center">Made by <a href="https://github.com/Thirumalai15"  target="_blank" class="text-center text-gray-400 hover:text-[#FF003C] cursor-pointer"><u>Thirumalai</u></a>, Mahesh, Jabilo Jose.</p>
-                </div>
-            <span class="inline-flex sm:ml-auto sm:mt-0 mt-2 justify-center sm:justify-start">
+            </div>
+            <div class="text-center flex-grow lg:my-0 my-3">
+                <p class="text-gray-400 text-sm sm:justify-center">Made by <a href="https://github.com/Thirumalai15"
+                        target="_blank"
+                        class="text-center text-gray-400 hover:text-[#FF003C] cursor-pointer"><u>Thirumalai</u></a>,
+                    Mahesh, Jabilo Jose.</p>
+            </div>
+            <span class="inline-flex sm:ml-auto sm:mt-0  justify-center sm:justify-start">
 
-                <a href="https://instagram.com/quintessence_ece?igshid=YmMyMTA2M2Y=" target="_blank" class="ml-3 text-gray-400 hover:text-red-500 cursor-pointer">
+                <a href="https://instagram.com/quintessence_ece?igshid=YmMyMTA2M2Y=" target="_blank"
+                    class="ml-3 text-gray-400 hover:text-red-500 cursor-pointer">
                     <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
                         stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
                         <rect width="20" height="20" x="2" y="2" rx="5" ry="5">
@@ -18,20 +22,24 @@
                         <path d="M16 11.37A4 4 0 1112.63 8 4 4 0 0116 11.37zm1.5-4.87h.01"></path>
                     </svg>
                 </a>
-                <a href="https://wa.me/+919500068698" target="_blank" class="ml-3 text-gray-400   hover:text-green-500 cursor-pointer">
+                <a href="https://wa.me/+919500068698" target="_blank"
+                    class="ml-3 text-gray-400   hover:text-green-500 cursor-pointer">
                     <svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5" width="28" height="28"
-                        viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" fill="none" stroke-linecap="round"
-                        stroke-linejoin="round">
+                        viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" fill="none"
+                        stroke-linecap="round" stroke-linejoin="round">
                         <path stroke="none" d="M0 0h24v24H0z" fill="none" />
                         <path d="M3 21l1.65 -3.8a9 9 0 1 1 3.4 2.9l-5.05 .9" />
                         <path
                             d="M9 10a0.5 .5 0 0 0 1 0v-1a0.5 .5 0 0 0 -1 0v1a5 5 0 0 0 5 5h1a0.5 .5 0 0 0 0 -1h-1a0.5 .5 0 0 0 0 1" />
                     </svg>
                 </a>
-                <a href="mailto: quintessence.eec@gmail.com" target="_blank" class="ml-3 text-gray-400   hover:text-yellow-500 cursor-pointer">
-                    <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-                        <path stroke-linecap="round" stroke-linejoin="round" d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" />
-                      </svg>
+                <a href="mailto: quintessence.eec@gmail.com" target="_blank"
+                    class="ml-3 text-gray-400   hover:text-yellow-500 cursor-pointer">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24"
+                        stroke="currentColor" stroke-width="2">
+                        <path stroke-linecap="round" stroke-linejoin="round"
+                            d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" />
+                    </svg>
                 </a>
 
             </span>
